c231029b44a142362e1d7e98043ddaf432d8f757
redcat243 <henri.peter.noval@gmail.com>
2026-04-23 09:26:12 -0500

    Update cat.sh-server.sh: refine server startup messages and streamline directory setup

--- cat.sh-server.sh
+++ cat.sh-server.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# cat-server.sh
 printf '\033[8;24;91t'
 echo -e "\033[0;32m"
 clear
-echo "--- CAT.SH SERVER CORE ---"
-mkdir -p ~/Desktop/cat.folder
-cd ~/Desktop/cat.folder
-
-echo "Server starting on port 8000..."
-echo "IP Address: $(ipconfig getifaddr en0)"
-# Starts a server that allows file browsing and downloading
+echo "--- CAT.SH DATA SERVER ---"
+echo "Listening for connections on port 8000..."
+echo "Your IP is: $(ipconfig getifaddr en0)"
+mkdir -p ~/Desktop/cat.folder && cd ~/Desktop/cat.folder
 python3 -m http.server 8000
